4d8e410b0445f702a7a800d7b4ccbe5a2b72fbe8
Clézio <cleziojr9@gmail.com>
2025-06-29 20:13:48 -0300

    :white_check_mark: test: modifica configuração do cdn_resources

--- functions/functions.py
+++ functions/functions.py
@@ -309,7 +309,7 @@ class Rede:
     def exibe_rede(self, g: nx.DiGraph, cabecalho: str, nome_arquivo_rede: str) -> None:
 
         # Função criada para facilitar a criação paramétrica das visualizações utilizando pyviz por meio do paradigma orientado a objetos
-        rede = net.Network(height='600px', width='100%', heading=cabecalho, directed=True, neighborhood_highlight=True, select_menu=True, filter_menu=True, notebook=False, cdn_resources='remote')
+        rede = net.Network(height='600px', width='100%', heading=cabecalho, directed=True, neighborhood_highlight=True, select_menu=True, filter_menu=True, notebook=True, cdn_resources='local')
         rede.from_nx(g)
         rede.show_buttons(filter_=True)
         rede.show(nome_arquivo_rede)
