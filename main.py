0c4ee3fe16aa7e50074addf94e6b3f41f021d270
Clézio <cleziojr9@gmail.com>
2025-06-29 19:35:17 -0300

    :sparkles: feat: adiciona requirements.txt | :white_check_mark: test: importação de módulos

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-import sys
-from pathlib import Path
-from functions import Dados, Rede
+from functions import processa_dados, constroi_rede, calcula_estatisticas_rede, cria_subredes
 
-project_root = Path(__file__).resolve().parent
+dados = Dados(path_data=r"data")
+df = dados.processa_dados(2023)
 
-path_data = project_root / "data"
-path_code = project_root / "code"
+rede_comercial = Rede(df=df)
+rede_comercial.constroi_rede()
+rede_comercial.calcula_estatisticas_rede(10, 10)
+rede_comercial.cria_subredes(10)
 
-if str(path_code) not in sys.path:
-    sys.path.append(str(path_code))
-
-try:
-    
-    dados = Dados(path_data=path_data)
-    df = dados.processa_dados(2023)
-    
-    rede_comercial = Rede(df=df)
-    rede_comercial.constroi_rede()
-    rede_comercial.calcula_estatisticas_rede(10, 10)
-    rede_comercial.cria_subredes(10)
-    
-except ImportError as e:
-    print(f"Erro ao importar módulos: {e}")
-    print(f"Verifique se o caminho {path_code} contém o arquivo functions.py")
-except Exception as e:
-    print(f"Ocorreu um erro durante a execução: {e}")
